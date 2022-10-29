a9f64d1727a4ebe7d2aa388fabf06d402a9fb627
tanveer-98 <tnvrahmed98@gmail.com>
2022-10-29 20:53:36 +0530

    updated with atlas db

--- server/db/db.js
+++ server/db/db.js
@@ -4,4 +4,13 @@ const path = require('path')
 // console.log(path.resolve(__dirname, '../dev.env'))
 require('dotenv').config();
 // console.log(process.env.MONGODB_URL)
-mongoose.connect(process.env.MONGODB_URL);
+// mongoose.connect(process.env.MONGODB_URL);
+
+mongoose.connect(process.env.MONGODB_URL_ATLAS, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: true,
+  dbName: 'apex'
+})
+  .then(() => console.log('Connected to MongoDB...'))
+  .catch(err => console.error('Could not connect to MongoDB...'));
