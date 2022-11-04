05c32979659e97447d8031ca4440db420940c41b
tanveer-98 <tnvrahmed98@gmail.com>
2022-11-04 19:18:22 +0530

    removed lazyloading of imports as it's not supported by vite

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,67 +1,46 @@
-import  React,{ useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './input.css'
-import {Routes,Route} from 'react-router-dom'
-import Home from './routes/home'
-import Navbar from './routes/navbar'
-import Auth from './routes/auth'
-import Login from './routes/login'
-import Register from './routes/register'
-import ViewBlog from './components/ViewBlog'
-import Profile from './routes/profile'
-import {useLocation} from 'react-router-dom'
-import {useTransition , animated} from 'react-spring'
-import Contact from './routes/contact'
-import About from './routes/about'
-const LazyWrapper = (str:string) => React.lazy(()=>import(str));
+import React, { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./input.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./routes/home";
+import Navbar from "./routes/navbar";
+import Auth from "./routes/auth";
+import Login from "./routes/login";
+import Register from "./routes/register";
+import ViewBlog from "./components/ViewBlog";
+import Profile from "./routes/profile";
+import { useLocation } from "react-router-dom";
+import { useTransition, animated } from "react-spring";
+import Contact from "./routes/contact";
+import About from "./routes/about";
+const LazyWrapper = (str: string) => React.lazy(() => import(str));
 
-const LazyAbout  = LazyWrapper('./routes/about')
-const LazyContact = LazyWrapper('./routes/contact')
-import Write from './routes/write';
-const LazyLogin = LazyWrapper('./routes/login')
-const LazyRegister = LazyWrapper('./routes/register')
+const LazyAbout = LazyWrapper("./routes/about");
+const LazyContact = LazyWrapper("./routes/contact");
+import Write from "./routes/write";
+const LazyLogin = LazyWrapper("./routes/login");
+const LazyRegister = LazyWrapper("./routes/register");
 
-
-const Wrapper = ({children}:any)=>{
-  return <React.Suspense fallback="Loaading...">
-    {children}
-  </React.Suspense>
-}
+const Wrapper = ({ children }: any) => {
+  return <React.Suspense fallback="Loaading...">{children}</React.Suspense>;
+};
 
 function App() {
   return (
-   <Routes>
-  <Route index path="/" element = { <Auth />}/>
-   <Route path = '/app' element = { <Navbar/> }>
-     <Route  path="/app/home" element = { <Home/>}/>
-     <Route  path="/app/about" element = {
-   
-        <About/>
-    
-       }/>
-     <Route  path="/app/contact" element = {
-      
-         <Contact/>
-       
-       }/>
-     <Route  path="/app/write" element = { 
-<Write/>
-}/>
-     <Route  path ="/app/blog/:id" element = {<ViewBlog/>}/>  
-     <Route  path ="/app/profile" element = {<Profile/>}/>  
-   </Route>
-   <Route path = "login" element = { 
-  
-    <Login/>
-   
-   }/>
-   <Route path = "register" element = { 
-   
-<Register/>
-  
-    }/>
-   </Routes>
-  )
+    <Routes>
+      <Route index path="/" element={<Auth />} />
+      <Route path="/app" element={<Navbar />}>
+        <Route path="/app/home" element={<Home />} />
+        <Route path="/app/about" element={<About />} />
+        <Route path="/app/contact" element={<Contact />} />
+        <Route path="/app/write" element={<Write />} />
+        <Route path="/app/blog/:id" element={<ViewBlog />} />
+        <Route path="/app/profile" element={<Profile />} />
+      </Route>
+      <Route path="login" element={<Login />} />
+      <Route path="register" element={<Register />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
