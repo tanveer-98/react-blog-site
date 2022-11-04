819a59b7f3e01e052c005ae804cfcf217c6533e7
tanveer-98 <tnvrahmed98@gmail.com>
2022-11-04 15:43:43 +0530

    switch db , set cookie to none secure

--- server/routes/user.js
+++ server/routes/user.js
@@ -207,12 +207,12 @@ router.post('/users/getCookie', async ( req,res)=>{
         .status(200)
         .cookie('token',token, {
 
-             sameSite : 'strict', // set to strict for local developerment 
+             sameSite : 'none', // set to strict for local developerment 
              path: "/",
              expires : new Date( new Date().getTime() +  5000* 1000),
              httpOnly: true ,
             //  secure : true
-            secure : false// incase of development set to false else cookie wont be saved in postman
+            secure : true// incase of development set to false else cookie wont be saved in postman
         })
 
 
