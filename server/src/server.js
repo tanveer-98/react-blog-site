efcd2779e65d1e9015aae752cd5a0768f28a3d89
tanveer-98 <tnvrahmed98@gmail.com>
2022-11-25 10:34:39 +0530

    fix:origin changed

--- server/src/server.js
+++ server/src/server.js
@@ -12,7 +12,7 @@ dotenv.config();
 app.use(function(req, res, next) {  
     // res.header('Access-Control-Allow-Origin', req.headers.origin);
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Headers","*");
+    // res.header("Access-Control-Allow-Headers","*");
     res.header('Access-Control-Allow-Credentials', true);
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     next();
